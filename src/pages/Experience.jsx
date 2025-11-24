fb7c63b47783cffcce2dc95d2dcf2ad67e926a19
mushawwir <mushawwirrd7@gmail.com>
2025-11-24 07:35:47 +0700

    fixing

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -73,7 +73,7 @@ export default function Experience({ data }) {
                         transition={{ duration: 1 }}
                         viewport={{ once: true }}
                         className=" flex justify-center mt-10">
-                        <OutLineButton icon2={<ArrowUpRightIcon />} href="https://drive.google.com/file/d/1d_Sy0sedcOHdNSgRTK3ZL-Zq-n96rDlg/view?usp=drivesdk " lable="My Resume" />
+                        <OutLineButton icon2={<ArrowUpRightIcon />} href="https://drive.google.com/file/d/1-4fzFVcOW51sCo_KAsS1glFCeLJ6lWiu/view?usp=drivesdk" lable="My Resume" />
                     </motion.div>
 
                 </div>
